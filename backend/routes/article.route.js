7ed63ad5a3cfb30de6f402d9f3da147a5c531dfd
ChaimaAraibi <chaima.araibi@insat.u-carthage.tn>
2020-07-08 11:21:51 +0200

    second commit delete, update, get by id

--- backend/routes/article.route.js
+++ backend/routes/article.route.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const articleRoutes = express.Router()
-const cors = require('cors')
 const article = require('../models/article.model')
 
 
@@ -15,6 +14,14 @@ articleRoutes.route('/').get(function(req, res) {
 });
 
 
+
+articleRoutes.route('/:id').get((req, res) => {
+    console.log(res)
+    article.findById(req.params.id)
+      .then(article => res.json(article))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
 articleRoutes.route('/addArticle').post(function(req, res) {
     let newArticle = new article (req.body);
     newArticle.save()
@@ -28,7 +35,39 @@ articleRoutes.route('/addArticle').post(function(req, res) {
 
 
 
+articleRoutes.route('/update/:id').post(function(req, res) {
+    article.findById(req.params.id, function(err, todo) {
+        if (!article)
+            res.status(404).send("data is not found");
+        else
+            article.title = req.body.title;
+            article.body = req.body.body;
+            
 
+            article.save().then(article => {
+                res.json('Article updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+    });
+});
+
+
+
+
+
+articleRoutes.route('/delete/:id').delete((req, res, next) => {
+    article.findByIdAndRemove(req.params.id, (error, data) => {
+      if (error) {
+        return next(error);
+      } else {
+        res.status(200).json({
+          msg: data
+        })
+      }
+    })
+  })
 
 
 module.exports = articleRoutes
